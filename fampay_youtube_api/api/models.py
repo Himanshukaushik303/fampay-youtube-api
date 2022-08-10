fd70bdede78713b17340fed31952919443648543
Himanshukaushik303 <kaushikhimanshu303@gmail.com>
2022-08-10 13:29:51 +0530

    Created Model

--- fampay_youtube_api/api/models.py
+++ fampay_youtube_api/api/models.py
@@ -1,5 +1,12 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
 class Videos(models.Model):
-    pass
+    title = models.TextField(blank =True)
+    thumbnails = models.JSONField(default=dict,blank=True)
+    channelId = models.CharField(max_length=256,blank=True)
+    channelTitle = models.TextField(blank=True)
+    publishTime = models.DateTimeField()
+
+    
