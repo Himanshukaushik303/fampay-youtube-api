065b96f42955f45ee1a88194eb7dac2ff6699b2d
Himanshukaushik303 <kaushikhimanshu303@gmail.com>
2022-08-10 21:50:33 +0530

    Added filter/ordering in API

--- fampay_youtube_api/api/views.py
+++ fampay_youtube_api/api/views.py
@@ -2,11 +2,12 @@ from rest_framework import filters
 from .models import *
 from .serializers import *
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 
 class ListVideos(generics.ListAPIView):
     search_fields = ["title", "description"]
-    filter_backends = (filters.SearchFilter,)
+    filter_backends = (filters.SearchFilter,filters.OrderingFilter)
     filterset_fields = ["channelTitle", "channelId"]
     queryset = Videos.objects.all()
     serializer_class = VideosSerializer
